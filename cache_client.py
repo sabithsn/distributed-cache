68f08311579925c80253789aacb53d99f1dad273
Sabit <sabithassan64@gmail.com>
2023-04-22 19:15:43 -0400

    client added

---
+++ cache_client.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import time
+
+request_type = "database"
+request_type = "static"
+
+if request_type == "database":
+
+    data = {
+        'title': 'How to Pick a Stage Name3'
+    }
+
+    misses = 0
+    start_time = time.time()
+
+    for i in range (100):
+        response = requests.post('http://localhost:8000/db_query/', data=data)
+        rcvd_data = response.json()
+
+        if response.status_code == 200:
+            misses += rcvd_data["miss"]
+
+            # print('POST request successful!')
+            # print ("rcvd_data:", rcvd_data)
+        else:
+            print('POST request failed!')
+
+    end_time = time.time()
+
+    print ("time taken:", end_time-start_time)
+    print ("cache hits:", 100-misses)
+    print ("cache misses:", misses)
+
+else:
+
+    misses = 0
+    start_time = time.time()
+
+    for i in range (100):
+        response = requests.post('http://localhost:8000/')
+        rcvd_data = response.json()
+
+        if response.status_code == 200:
+            misses += rcvd_data["miss"]
+
+            # print('POST request successful!')
+            # print ("rcvd_data:", rcvd_data)
+        else:
+            print('POST request failed!')
+
+    end_time = time.time()
+
+    print ("time taken:", end_time-start_time)
+    print ("cache hits:", 100-misses)
+    print ("cache misses:", misses)
